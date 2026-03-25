bffa2c286762d5c2dc0dcc3584c6c38af48394c0
bleronamaxhuni <74790174+bleronamaxhuni@users.noreply.github.com>
2026-03-25 12:03:59 +0100

    fixes on js files

---
+++ public/build/assets/app-BSBy97CF.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o,a as r}from"./_plugin-vue_export-helper-BQ5-8BMs.js";const a={};function n(p,s){return o(),c("h1",null,"Hello from Vue + Laravel")}const t=e(a,[["render",n]]);r(t).mount("#app");
